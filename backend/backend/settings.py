fa12d14c6f64ce0288064918d99f8419bcbd8326
sashalesik <shirikova.alexandra@gmail.com>
2025-06-21 23:27:51 +0300

    test666

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / "backend_static" / "static"
-
+# STATIC_ROOT = BASE_DIR / "backend_static" / "static"
+STATIC_ROOT = BASE_DIR / "backend_static" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
