1c9e5f9d40b6d26caa1a0e369a9ac6232f02831a
sashalesik <shirikova.alexandra@gmail.com>
2025-06-22 13:21:38 +0300

    test666

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / "backend_static" 
+STATIC_ROOT = BASE_DIR / "backend_static"
 # STATIC_ROOT = BASE_DIR / "backend_static"
 
 # Default primary key field type
